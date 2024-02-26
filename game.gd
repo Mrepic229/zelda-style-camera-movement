5d044702c855f2ec3980e6c2e782ac4a10b5b534
David <jis69508@gmail.com>
2024-02-25 19:23:25 -0600

    screen moves as player goes to a side

--- game.gd
+++ game.gd
@@ -13,3 +13,19 @@ func _process(delta):
 
 func _on_area_2d_body_entered(body):
 	$Camera2D.position.x += 960
+
+
+func _on_player_visible_notifier_screen_exited():
+	
+	# following code moves camera if player goes offscreen
+	var player_location_to_camera = $player.position - $Camera2D.position
+	if player_location_to_camera.x > get_viewport_rect().size.x:
+		$Camera2D.position.x += get_viewport_rect().size.x
+	if player_location_to_camera.x < 0:
+		$Camera2D.position.x -= get_viewport_rect().size.x
+	if player_location_to_camera.y > get_viewport_rect().size.y:
+		$Camera2D.position.y += get_viewport_rect().size.y
+	if player_location_to_camera.y < 0:
+		$Camera2D.position.y -= get_viewport_rect().size.y
+		
+
